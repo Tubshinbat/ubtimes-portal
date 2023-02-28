e800c912828017c860484a5b3541c123a6ce8565
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-02-28 16:43:51 +0800

    base url change

--- axios-base.js
+++ axios-base.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://ubtimes.mn/api/",
+  baseURL: "https://ubtimes.mn/api/",
 });
 
 instance.defaults.withCredentials = true;
